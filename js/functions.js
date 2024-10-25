7cdfc5d09d6ed9f0a5a514b7499c895baf815cff
anguianoo <njanguiano@gmail.com>
2024-10-25 09:52:59 -0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,16 @@
 /* define your functions here */
 
+function outputCartRow(item) {
+    const { product, quantity } = item;
+    const total = calculateTotal(quantity, product.price);
 
-
-
-
-
-
-
-        
+    document.write(`
+        <tr>
+            <td><img src="project2/images/${product.filename}" alt="${product.title}" class="painting"></td>
+            <td>${product.title}</td>
+            <td>${quantity}</td>
+            <td>$${product.price.toFixed(2)}</td>
+            <td>$${total}</td>
+        </tr>
+    `);
+}
