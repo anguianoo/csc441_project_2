31eea8efa5626ef6ffbaeaf7da4f2e482a278e6e
anguianoo <njanguiano@gmail.com>
2024-10-25 09:53:25 -0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,9 @@
 /* define your functions here */
 
+function calculateTotal(quantity, price) {
+    return (quantity * price).toFixed(2); // Keeps the result to 2 decimal places
+}
+
 function outputCartRow(item) {
     const { product, quantity } = item;
     const total = calculateTotal(quantity, product.price);
